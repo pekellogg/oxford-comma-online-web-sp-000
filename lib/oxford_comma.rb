08222b40f965ccfb25574ae7723172225297f7d2
pekellogg <pekellogg@hotmail.com>
2021-04-19 17:49:30 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    elsif array.size == 3
+        "#{array[0]}, #{array[1]}, and #{array[2]}"
+    elsif array.size >= 4
+        array[0..-2].join(", ") + ", and " + array[-1]
+    end
 end
